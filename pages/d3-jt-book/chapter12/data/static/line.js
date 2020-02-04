1b3c57a9de1ac60be0a9c3840ca6ccfb4e23a56d
hz-qq2 <601661706@qq.com>
2020-02-04 10:00:29 +0800

    point

--- pages/d3-jt-book/chapter12/data/static/line.js
+++ pages/d3-jt-book/chapter12/data/static/line.js
@@ -1,5 +1,5 @@
 
-export const renderLine =({svg, linePath, colors, dataset, padding})=>{
+export const renderLine =({svg, linePath, colors, dataset, padding,xScale,yScale})=>{
 //添加路径
 svg.selectAll("path")	//选择<svg>中所有的<path>
 	.data(dataset)		//绑定数据
@@ -14,4 +14,19 @@ svg.selectAll("path")	//选择<svg>中所有的<path>
 	.attr("stroke",function(d,i){
 		return colors[i];
 	});
+
+	dataset.forEach(item => {
+		item.gdp.forEach(point=>{
+		//从数据中获取年份和GDP值
+		var x1 = point[0];
+		var y1 = point[1];
+		//分别用x轴和y轴的比例尺，计算焦点的位置
+		var focusX = xScale(x1) + padding.left;
+		var focusY = yScale(y1) + padding.top;
+				svg.append("circle")
+				.attr("r", 4.5)
+				.attr("transform","translate(" + focusX + "," + focusY + ")");
+			})
+	});
+	
 }
