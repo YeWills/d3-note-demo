e5d04aa0db42b11dd512894159ddd929eaf781df
hz-qq2 <601661706@qq.com>
2020-02-07 19:35:38 +0800

    init modify

---
+++ pages/sar-chart-modify/static/axis.js
@@ -0,0 +1,104 @@
+
+class AxisFn{
+	constructor({dataset, padding, width, height}){
+		this.width=width;
+		this.height=height;
+		this.dataset=dataset;
+		this.padding=padding;
+		this.xDomain=dataset.length ? dataset[0].gdp.length-1 : 0;
+		return;
+	}
+	generateXscale=(width, padding)=>{
+		const xScale = d3.scale.linear()
+					.domain([0,this.xDomain])
+					.range([ 0 , width - padding.left - padding.right ]);
+		return xScale;
+	}
+	generateYscale=(height,padding,dataset)=>{
+		const gdpmax = this.getYmax(dataset);
+		const yScale = d3.scale.linear()
+					.domain([0,gdpmax * 1.1])
+					.range([ height - padding.top - padding.bottom , 0 ]);
+		return yScale;
+	}
+	getYmax=(dataset)=>{
+		let gdpmax = 0;
+		for(let i = 0; i < dataset.length; i++){
+			const currGdp = d3.max( dataset[i].gdp , function(d) { return d[1]; } );
+			if( currGdp > gdpmax ){
+				gdpmax = currGdp;
+			}
+		}
+		return gdpmax;
+	}
+	generateLinePath=(xScale, yScale)=>{
+		//创建一个直线生成器
+		var linePath = d3.svg.line()
+		.x(function(d){ return xScale(d[2]); })
+		.y(function(d){ return yScale(d[1]); })
+		//.interpolate("basis");
+		return linePath;
+	}
+	init=()=>{
+		const xScale = this.generateXscale(this.width, this.padding);
+		const yScale = this.generateYscale(this.height, this.padding, this.dataset);
+		const linePath = this.generateLinePath(xScale, yScale);
+		return {
+			xScale,
+			yScale,
+			linePath,
+		}
+	}
+	renderXaxis = ({
+		svg,
+		xScale,
+		padding,
+		height
+	}) => {
+		//x轴
+		const xAxis = d3.svg.axis()
+			.scale(xScale)
+			// .ticks(5)
+			.tickFormat(function(a,b, c){
+				var startDate = '2020/1/24';
+				const month = moment(startDate, 'YYYY/MM/DD').add(a, 'day').get('month')+1;
+				const day = moment(startDate, 'YYYY/MM/DD').add(a, 'day').get('date');
+				return `${month}/${day}`;
+			})
+			.orient("bottom");
+		var update = svg.selectAll("g.xAxis")
+			//data 后面的数组[1]只是任意写的，只要数组的长度为1就行，使用data，以便得到 enter、exit用来重绘。
+			.data([1]);
+		var enter = update.enter();
+		var exit = update.exit();
+		update.call(xAxis);
+		enter.append("g")
+			.attr("class", "xAxis axis")
+			.attr("transform", "translate(" + padding.left + "," + (height - padding.bottom) + ")")
+			.call(xAxis);
+		exit.remove();
+
+	}
+	renderYaxis = ({
+		svg,
+		yScale,
+		padding
+	}) => {
+		//y轴
+		const yAxis = d3.svg.axis()
+			.scale(yScale)
+			.orient("left");
+		var update = svg.selectAll("g.yAxis")
+			.data([1]);
+		var enter = update.enter();
+		var exit = update.exit();
+		update.call(yAxis);
+		enter.append("g")
+			.attr("class", "yAxis axis")
+			.attr("transform", "translate(" + padding.left + "," + padding.top + ")")
+			.call(yAxis);
+		exit.remove();
+	}
+	}
+
+export const Axis = AxisFn
---
+++ pages/sar-chart-modify/static/config.js
@@ -0,0 +1,97 @@
+import {chartDatas} from './data.js';
+
+const width  = 800;	//SVG绘制区域的宽度
+const height = 600;	//SVG绘制区域的高度
+
+var dataset = [
+	{
+		country: "确诊",
+		type:'sure',
+		gdp:[]
+	},
+	{
+		country: "疑似",
+		type:'suspected',
+		gdp:[]
+	},
+	{
+		country: "治愈",
+		type:'cure',
+		gdp:[]
+	},
+	{
+		country: "死亡",
+		type:'over',
+		gdp:[]
+	},
+	{
+		country: "新增",
+		type:'new',
+		gdp:[]
+	},
+	{
+		country: "新增疑似",
+		type:'newsp',
+		gdp:[]
+	}
+];
+const typeMap = ['sure', 'suspected', 'cure', 'over', 'new', 'newsp']
+dataset = dataset.map((t,i)=>{
+	t.gdp = Object.entries(chartDatas).map(item=>{
+		return [item[0], item[1][typeMap[i]]]
+	})
+	return t;
+})
+
+dataset = dataset.map(item=>{
+	item.gdp = item.gdp.map((cell, index)=>{
+		cell = cell.concat([index, item.type]);
+		return cell;
+	})
+	return item;
+})
+
+
+const handleColor=(d, colors)=>{
+	switch(d.type){
+		case 'sure':{
+			return colors[0];
+			break;
+		}
+		case 'suspected':{
+			return colors[1];
+			break;
+		}
+		case 'cure':{
+			return colors[2];
+			break;
+		}
+		case 'over':{
+			return colors[3];
+			break;
+		}
+		case 'new':{
+			return colors[4];
+			break;
+		}
+		case 'newsp':{
+			return colors[5];
+			break;
+		}
+		default:{}
+	}
+}
+
+//定义两个颜色				
+// var colors = [ d3.rgb(0,0,255) , d3.rgb(0,255,0), d3.rgb(0,255,255), d3.rgb(255,255,0) ];
+var colors = [ 'rgb(232, 49, 50)' , 'rgb(236, 146, 23)', 'rgb(16, 174, 181)', 'rgb(77, 80, 84)', 'rgba(10, 45, 237, 0.88)', '#9C27B0' ];
+//外边框
+var padding = { top: 50 , right: 50, bottom: 50, left: 50 };
+export {
+    width,
+    height,
+    padding,
+    dataset,
+    colors,
+    handleColor,
+}
