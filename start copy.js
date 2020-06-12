1b638cead12da18b88a7dd55ed0dcc85574591e5
hz-qq2 <601661706@qq.com>
2020-06-12 08:03:39 +0800

    test

---
+++ start copy.js
@@ -0,0 +1,516 @@
+[ { id: 'chapter10', pId: 0, name: 'chapter10' },
+  { id: '10.1', pId: 'chapter10', name: '10.1' },
+  { id: 'chapter10/10.1/10-1-pie.html',
+    pId: '10.1',
+    name: '10-1-pie.html' },
+  { id: 'chapter10', pId: 0, name: 'chapter10' },
+  { id: '10.2', pId: 'chapter10', name: '10.2' },
+  { id: 'chapter10/10.2/10-2-1-force.html',
+    pId: '10.2',
+    name: '10-2-1-force.html' },
+  { id: 'chapter10', pId: 0, name: 'chapter10' },
+  { id: '10.2', pId: 'chapter10', name: '10.2' },
+  { id: 'chapter10/10.2/10-2-2-drag-fixed.html',
+    pId: '10.2',
+    name: '10-2-2-drag-fixed.html' },
+  { id: 'chapter10', pId: 0, name: 'chapter10' },
+  { id: '10.3', pId: 'chapter10', name: '10.3' },
+  { id: 'chapter10/10.3/10-3-1-chord.html',
+    pId: '10.3',
+    name: '10-3-1-chord.html' },
+  { id: 'chapter10', pId: 0, name: 'chapter10' },
+  { id: '10.3', pId: 'chapter10', name: '10.3' },
+  { id: 'chapter10/10.3/10-3-2-chord-interactive.html',
+    pId: '10.3',
+    name: '10-3-2-chord-interactive.html' },
+  { id: 'chapter10', pId: 0, name: 'chapter10' },
+  { id: '10.4', pId: 'chapter10', name: '10.4' },
+  { id: 'chapter10/10.4/10-4-1-tree.html',
+    pId: '10.4',
+    name: '10-4-1-tree.html' },
+  { id: 'chapter10', pId: 0, name: 'chapter10' },
+  { id: '10.5', pId: 'chapter10', name: '10.5' },
+  { id: 'chapter10/10.5/10-5-1-bundle.html',
+    pId: '10.5',
+    name: '10-5-1-bundle.html' },
+  { id: 'chapter10', pId: 0, name: 'chapter10' },
+  { id: '10.6', pId: 'chapter10', name: '10.6' },
+  { id: 'chapter10/10.6/10-6-1-histogram.html',
+    pId: '10.6',
+    name: '10-6-1-histogram.html' },
+  { id: 'chapter10', pId: 0, name: 'chapter10' },
+  { id: '10.7', pId: 'chapter10', name: '10.7' },
+  { id: 'chapter10/10.7/10-7-1-treemap.html',
+    pId: '10.7',
+    name: '10-7-1-treemap.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.2', pId: 'chapter11', name: '11.2' },
+  { id: '11.2.1', pId: '11.2', name: '11.2.1' },
+  { id: 'chapter11/11.2/11.2.1/11-2-1-GeoJSON-chinamap.html',
+    pId: '11.2.1',
+    name: '11-2-1-GeoJSON-chinamap.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.2', pId: 'chapter11', name: '11.2' },
+  { id: '11.2.2', pId: '11.2', name: '11.2.2' },
+  { id: '11.2.2.1', pId: '11.2.2', name: '11.2.2.1' },
+  { id: 'chapter11/11.2/11.2.2/11.2.2.1/11-2-2-1-TopoJSON-chinamap.html',
+    pId: '11.2.2.1',
+    name: '11-2-2-1-TopoJSON-chinamap.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.2', pId: 'chapter11', name: '11.2' },
+  { id: '11.2.2', pId: '11.2', name: '11.2.2' },
+  { id: '11.2.2.2', pId: '11.2.2', name: '11.2.2.2' },
+  { id: 'chapter11/11.2/11.2.2/11.2.2.2/12-2-2-2-merge.html',
+    pId: '11.2.2.2',
+    name: '12-2-2-2-merge.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.2', pId: 'chapter11', name: '11.2' },
+  { id: '11.2.2', pId: '11.2', name: '11.2.2' },
+  { id: '11.2.2.3', pId: '11.2.2', name: '11.2.2.3' },
+  { id: 'chapter11/11.2/11.2.2/11.2.2.3/12-2-2-3-mesh.html',
+    pId: '11.2.2.3',
+    name: '12-2-2-3-mesh.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.2', pId: 'chapter11', name: '11.2' },
+  { id: '11.2.2', pId: '11.2', name: '11.2.2' },
+  { id: '11.2.2.4', pId: '11.2.2', name: '11.2.2.4' },
+  { id: 'chapter11/11.2/11.2.2/11.2.2.4/12-2-2-4-neighbors.html',
+    pId: '11.2.2.4',
+    name: '12-2-2-4-neighbors.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.3', pId: 'chapter11', name: '11.3' },
+  { id: '11.3.1', pId: '11.3', name: '11.3.1' },
+  { id: 'chapter11/11.3/11.3.1/11-3-1-geopath.html',
+    pId: '11.3.1',
+    name: '11-3-1-geopath.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.3', pId: 'chapter11', name: '11.3' },
+  { id: '11.3.2', pId: '11.3', name: '11.3.2' },
+  { id: 'chapter11/11.3/11.3.2/11-3-2-1-grid-world.html',
+    pId: '11.3.2',
+    name: '11-3-2-1-grid-world.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.3', pId: 'chapter11', name: '11.3' },
+  { id: '11.3.2', pId: '11.3', name: '11.3.2' },
+  { id: 'chapter11/11.3/11.3.2/11-3-2-2-grid-china.html',
+    pId: '11.3.2',
+    name: '11-3-2-2-grid-china.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.3', pId: 'chapter11', name: '11.3' },
+  { id: '11.3.2', pId: '11.3', name: '11.3.2' },
+  { id: 'chapter11/11.3/11.3.2/11-3-2-3-circle-grid.html',
+    pId: '11.3.2',
+    name: '11-3-2-3-circle-grid.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.3', pId: 'chapter11', name: '11.3' },
+  { id: '11.3.2', pId: '11.3', name: '11.3.2' },
+  { id: 'chapter11/11.3/11.3.2/11-3-2-4-sphere-grid.html',
+    pId: '11.3.2',
+    name: '11-3-2-4-sphere-grid.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.4', pId: 'chapter11', name: '11.4' },
+  { id: 'chapter11/11.4/11-4-1-set-beijing.html',
+    pId: '11.4',
+    name: '11-4-1-set-beijing.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.4', pId: 'chapter11', name: '11.4' },
+  { id: 'chapter11/11.4/11-4-2-set-london.html',
+    pId: '11.4',
+    name: '11-4-2-set-london.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.4', pId: 'chapter11', name: '11.4' },
+  { id: 'chapter11/11.4/11-4-3-mark-washington.html',
+    pId: '11.4',
+    name: '11-4-3-mark-washington.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.4', pId: 'chapter11', name: '11.4' },
+  { id: 'chapter11/11.4/11-4-4-clip-right.html',
+    pId: '11.4',
+    name: '11-4-4-clip-right.html' },
+  { id: 'chapter11', pId: 0, name: 'chapter11' },
+  { id: '11.4', pId: 'chapter11', name: '11.4' },
+  { id: 'chapter11/11.4/11-4-5-rotate.html',
+    pId: '11.4',
+    name: '11-4-5-rotate.html' },
+  { id: 'chapter12', pId: 0, name: 'chapter12' },
+  { id: '12.1', pId: 'chapter12', name: '12.1' },
+  { id: 'chapter12/12.1/12-1-1-tooltip.html',
+    pId: '12.1',
+    name: '12-1-1-tooltip.html' },
+  { id: 'chapter12', pId: 0, name: 'chapter12' },
+  { id: '12.1', pId: 'chapter12', name: '12.1' },
+  { id: 'chapter12/12.1/12-1-2-tooltip-style.html',
+    pId: '12.1',
+    name: '12-1-2-tooltip-style.html' },
+  { id: 'chapter12', pId: 0, name: 'chapter12' },
+  { id: '12.1', pId: 'chapter12', name: '12.1' },
+  { id: 'chapter12/12.1/12-1-2-tooltip-style2.html',
+    pId: '12.1',
+    name: '12-1-2-tooltip-style2.html' },
+  { id: 'chapter12', pId: 0, name: 'chapter12' },
+  { id: '12.2', pId: 'chapter12', name: '12.2' },
+  { id: 'chapter12/12.2/12-2-1-focusLine.html',
+    pId: '12.2',
+    name: '12-2-1-focusLine.html' },
+  { id: 'chapter12', pId: 0, name: 'chapter12' },
+  { id: '12.2', pId: 'chapter12', name: '12.2' },]
+
+
+
+
+
+
+
+    [ '.DS_Store',
+  'chapter10',
+  'chapter11',
+  'chapter12',
+  'chapter13',
+  'chapter15',
+  'chapter16',
+  'chapter2',
+  'chapter4',
+  'chapter5',
+  'chapter6',
+  'chapter7',
+  'chapter8',
+  'chapter9',
+  'd3',
+  'file.json',
+  'index.html' ]
+[ '10.1', '10.2', '10.3', '10.4', '10.5', '10.6', '10.7' ]
+[ '10-1-pie.html' ]
+[ '10-2-1-force.html', '10-2-2-drag-fixed.html' ]
+[ '10-3-1-chord.html', '10-3-2-chord-interactive.html' ]
+[ '10-4-1-tree.html', 'city.json' ]
+[ '10-5-1-bundle.html' ]
+[ '10-6-1-histogram.html' ]
+[ '10-7-1-treemap.html', 'citygdp.json' ]
+[ '11.2', '11.3', '11.4' ]
+[ '11.2.1', '11.2.2' ]
+[ '11-2-1-GeoJSON-chinamap.html',
+  'china.geojson',
+  'southchinasea.svg' ]
+[ '11.2.2.1', '11.2.2.2', '11.2.2.3', '11.2.2.4' ]
+[ '11-2-2-1-TopoJSON-chinamap.html',
+  'china.topojson',
+  'southchinasea.svg' ]
+[ '12-2-2-2-merge.html', 'china.topojson', 'southchinasea.svg' ]
+[ '12-2-2-3-mesh.html', 'china.topojson', 'southchinasea.svg' ]
+[ '12-2-2-4-neighbors.html',
+  'china.topojson',
+  'southchinasea.svg' ]
+[ '11.3.1', '11.3.2' ]
+[ '11-3-1-geopath.html', 'china.topojson', 'southchinasea.svg' ]
+[ '11-3-2-1-grid-world.html',
+  '11-3-2-2-grid-china.html',
+  '11-3-2-3-circle-grid.html',
+  '11-3-2-4-sphere-grid.html',
+  'china.topojson',
+  'southchinasea.svg',
+  'world_605kb.json' ]
+[ '11-4-1-set-beijing.html',
+  '11-4-2-set-london.html',
+  '11-4-3-mark-washington.html',
+  '11-4-4-clip-right.html',
+  '11-4-5-rotate.html',
+  'world_605kb.json' ]
+[ '.DS_Store', '12.1', '12.2', '12.3', '12.4', '12.5', 'data' ]
+[ '12-1-1-tooltip.html',
+  '12-1-2-tooltip-style.html',
+  '12-1-2-tooltip-style2.html' ]
+[ '12-2-1-focusLine.html',
+  '12-2-2-tooltip.html',
+  'data.html',
+  'data.js',
+  'data.json' ]
+[ '12-3-1-pie-drag.html',
+  '12-3-1_1-pie-drag.html',
+  '12-3-2-merge-split.html',
+  '12-3-3-combination.html' ]
+[ '12-4-1-selectArea.html', '12-4-2-selectArea-scatter.html' ]
+[ '12-5-1-switch.html', 'learn.json' ]
+[ '.DS_Store', 'index.html', 'lib', 'static', 'style.css' ]
+[ 'moment.min.js' ]
+[ 'axis.js', 'config.js', 'line.js', 'tooltip.js' ]
+[ '13.1', '13.2', '13.3', '13.4', '13.5' ]
+[ '13-1-colorRange.html',
+  'china.topojson',
+  'southchinasea.svg',
+  'tourism.json' ]
+[ '13-2-1-mark-place.html',
+  '13-2-2-night-light.html',
+  'china.topojson',
+  'cities.json',
+  'img',
+  'places.json',
+  'southchinasea.svg' ]
+[ 'beijing.png',
+  'guilin.png',
+  'lasa.png',
+  'shanghai.png',
+  'wulumuqi.png' ]
+[ '13-3-1-arrow-line.html',
+  '13-3-2-sphere-line.html',
+  'china.topojson',
+  'southchinasea.svg',
+  'world_605kb.json' ]
+[ '13-4-1-plane-drag-scale.html',
+  '13-4-2-globe-drag-scale.html',
+  'world.json' ]
+[ '13-5-1-triangulation.html',
+  '13-5-1-voronoi.html',
+  '13-5-2-force-directed-map.html',
+  'china.geojson' ]
+[ '010_最少的代码制作力导向图',
+  '020_重力',
+  '030_弹性力',
+  '040_万有引力',
+  '050_碰撞力',
+  '060_理解Alpha',
+  '070_前进和停止',
+  '080_为canvas添加拖放事件',
+  '090_完整的力导向图',
+  '100_配色模板',
+  '110_d3-color',
+  '120_线段生成器的版本差异',
+  '130_折线图和线段生成器',
+  '140_对角线生成器',
+  '150_更简单的坐标轴',
+  '160_堆栈图的数据结构',
+  '170_从思维导图来探究d3-hierarchy',
+  '171_节点的最短路径',
+  '180_打包图',
+  '190_单元素的无限过渡',
+  '200_多元素的无限过渡' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'data.csv', 'index.html' ]
+[ 'data.csv', 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ 'index.html' ]
+[ '010_描边和填充',
+  '020_选择区域',
+  '030_拖动和缩放',
+  '040_Canvas的流星效果',
+  '050_地图上飞舞的流星',
+  '060_形状生成器',
+  '070_球体地图的旋转和缩放',
+  '080_Leaflet和D3' ]
+[ 'data', 'index.html', 'js' ]
+[ 'china.topojson',
+  'southchinasea.svg',
+  'southchinasea_black.png',
+  'southchinasea_white.png' ]
+[ 'topojson.js' ]
+[ 'data', 'index.html', 'js' ]
+[ 'china.topojson',
+  'southchinasea.svg',
+  'southchinasea_black.png',
+  'southchinasea_white.png' ]
+[ 'topojson.js' ]
+[ 'data', 'index.html', 'js' ]
+[ 'china.topojson',
+  'southchinasea.svg',
+  'southchinasea_black.png',
+  'southchinasea_white.png' ]
+[ 'topojson.js' ]
+[ 'index.html' ]
+[ 'data', 'index.html', 'js' ]
+[ 'china.topojson',
+  'southchinasea.svg',
+  'southchinasea_black.png',
+  'southchinasea_white.png',
+  'southchinasea_yellow.png' ]
+[ 'topojson.js' ]
+[ 'data', 'index.html', 'js' ]
+[ 'world_map.json' ]
+[ 'topojson.js' ]
+[ 'data', 'index.html', 'js' ]
+[ 'world_map_31kb.json',
+  'world_map_386kb.json',
+  'world_map_98kb.json',
+  'world_map_origin.json' ]
+[ 'topojson.js' ]
+[ '1_载入地图',
+  '2_读入TopoJSON地图',
+  '3_加入标注',
+  '4_添加SVG在地图上',
+  '5_添加Canvas在地图上' ]
+[ 'images', 'index.html', 'leaflet.css', 'leaflet.js' ]
+[ 'layers-2x.png',
+  'layers.png',
+  'marker-icon-2x.png',
+  'marker-icon.png',
+  'marker-shadow.png' ]
+[ 'china.topojson',
+  'images',
+  'index.html',
+  'leaflet.css',
+  'leaflet.js',
+  'topojson.js' ]
+[ 'layers-2x.png',
+  'layers.png',
+  'marker-icon-2x.png',
+  'marker-icon.png',
+  'marker-shadow.png' ]
+[ 'images', 'index.html', 'leaflet.css', 'leaflet.js' ]
+[ 'layers-2x.png',
+  'layers.png',
+  'marker-icon-2x.png',
+  'marker-icon.png',
+  'marker-shadow.png' ]
+[ 'china.topojson',
+  'images',
+  'index.html',
+  'leaflet.css',
+  'leaflet.js',
+  'topojson.js' ]
+[ 'layers-2x.png',
+  'layers.png',
+  'marker-icon-2x.png',
+  'marker-icon.png',
+  'marker-shadow.png' ]
+[ 'china.topojson',
+  'images',
+  'index.html',
+  'leaflet.css',
+  'leaflet.js',
+  'topojson.js' ]
+[ 'layers-2x.png',
+  'layers.png',
+  'marker-icon-2x.png',
+  'marker-icon.png',
+  'marker-shadow.png' ]
+[ '2.5', '2.6' ]
+[ '2-5-2-1-rect.svg',
+  '2-5-2-2-circle.svg',
+  '2-5-2-3-line.svg',
+  '2-5-2-4-polygon.svg',
+  '2-5-2-5-path.svg',
+  '2-5-2-6-bezier3.svg',
+  '2-5-2-7-bezier2.svg',
+  '2-5-2-8-arc.svg',
+  '2-5-3-1-text.svg',
+  '2-5-5-1-marker.svg',
+  '2-5-6-1-blur.svg',
+  '2-5-7-1-gradient.svg' ]
+[ '2-6-3-1-basicShape.html',
+  '2-6-4-1-filltext.html',
+  '2-6-5-1-transform.html',
+  '2-6-6-1-image.html',
+  '2-6-7-1-gradient.html',
+  'd3icon.png' ]
+[ '4.8' ]
+[ '4-8-1-rect-text.html',
+  '4-8-2-update-data.html',
+  '4-8-test.html' ]
+[ '5.1', '5.2', '5.3', '5.4' ]
+[ '5-1-1-linearScale.html',
+  '5-1-2-powScale.html',
+  '5-1-3-quantizeScale.html',
+  '5-1-4-thresholdScale.html' ]
+[ '5-2-1-ordinalScale.html', '5-2-2-colorScale.html' ]
+[ '5-3-1-drawAxis.html',
+  '5-3-2-degree.html',
+  '5-3-3-manyAxes.html' ]
+[ '5-4-1-scatter.html' ]
+[ '6.1', '6.2', '6.3', '6.4', '6.5', '6.6', '6.7', '6.8' ]
+[ '6-1-1-RGB.html',
+  '6-1-2-HSL.html',
+  '6-1-3-interpolation.html' ]
+[ '6-2-1-lineGenerator.html',
+  '6-2-2-accessor.html',
+  '6-2-3-defined.html' ]
+[ '6-3-1-areaGenerator.html' ]
+[ '6-4-1-arcGenerator.html', '6-4-2-makePie.html' ]
+[ '6-5-1-symbolGenerator.html' ]
+[ '6-6-1-chordGenerator.html' ]
+[ '6-7-1-diagonalGenerator.html', '6-7-2-projection.html' ]
+[ '6-8-1-lineChart.html' ]
+[ '7.1', '7.2' ]
+[ '7-1-1-1-rectTransition.html',
+  '7-1-1-2-transitionObj.html',
+  '7-1-1-3-duration-delay.html',
+  '7-1-1-4-someTimes.html',
+  '7-1-2-1-attr-attrTween.html',
+  '7-1-2-2-style-styleTween.html',
+  '7-1-2-3-text-tween.html',
+  '7-1-3-childElement.html',
+  '7-1-4-1-transition-each.html',
+  '7-1-4-2-transition-call.html',
+  '7-1-5-ease.html' ]
+[ '7-2-scatter-transition.html' ]
+[ '8.1', '8.2', '8.3' ]
+[ '8-1-1-mouse.html', '8-1-2-keyboard.html', '8-1-3-touch.html' ]
+[ '8-2-1-d3.event.html' ]
+[ '8-3-1-drag.html',
+  '8-3-2-drag.origin.html',
+  '8-3-3-scale.html',
+  '8-3-4-scale2.html' ]
+[ '9.1', '9.2' ]
+[ '9-1-1-readjson.html',
+  '9-1-3-readxml.html',
+  '9-1-4-readtext.html',
+  'city.json',
+  'example.xml',
+  'note.txt' ]
+[ '9-2-1-d3-downloadable.html', 'd3-downloadable', 'd3.min.js' ]
+[ 'd3-downloadable.css', 'd3-downloadable.js' ]
+[ '.DS_Store', '3.x', '4.x' ]
+[ 'LICENSE', 'd3.js', 'd3.min.js', 'topojson.js' ]
+[ 'API.md',
+  'CHANGES.md',
+  'LICENSE',
+  'README.md',
+  'd3.js',
+  'd3.min.js' ]
+
+
+
+
+  [ 'chapter10/10.1/10-1-pie.html',
+  'chapter10/10.2/10-2-1-force.html',
+  'chapter10/10.2/10-2-2-drag-fixed.html',
+  'chapter10/10.3/10-3-1-chord.html',
+  'chapter10/10.3/10-3-2-chord-interactive.html',
+  'chapter10/10.4/10-4-1-tree.html',
+  'chapter10/10.5/10-5-1-bundle.html',
+  'chapter10/10.6/10-6-1-histogram.html',
+  'chapter10/10.7/10-7-1-treemap.html',
+  'chapter11/11.2/11.2.1/11-2-1-GeoJSON-chinamap.html',
+  'chapter11/11.2/11.2.2/11.2.2.1/11-2-2-1-TopoJSON-chinamap.html',
+  'chapter11/11.2/11.2.2/11.2.2.2/12-2-2-2-merge.html',
+  'chapter11/11.2/11.2.2/11.2.2.3/12-2-2-3-mesh.html',
+  'chapter11/11.2/11.2.2/11.2.2.4/12-2-2-4-neighbors.html',
+  'chapter11/11.3/11.3.1/11-3-1-geopath.html',
+  'chapter11/11.3/11.3.2/11-3-2-1-grid-world.html',
+  'chapter11/11.3/11.3.2/11-3-2-2-grid-china.html',
+  'chapter11/11.3/11.3.2/11-3-2-3-circle-grid.html',
+  'chapter11/11.3/11.3.2/11-3-2-4-sphere-grid.html',
+  'chapter11/11.4/11-4-1-set-beijing.html',
+  'chapter11/11.4/11-4-2-set-london.html',
+  'chapter11/11.4/11-4-3-mark-washington.html',
+  'chapter11/11.4/11-4-4-clip-right.html',
+  'chapter11/11.4/11-4-5-rotate.html',
+  'chapter12/12.1/12-1-1-tooltip.html',
+  'chapter12/12.1/12-1-2-tooltip-style.html',
+  'chapter12/12.1/12-1-2-tooltip-style2.html',
+  'chapter12/12.2/12-2-1-focusLine.html',
+  'chapter12/12.2/12-2-2-tooltip.html',
+
